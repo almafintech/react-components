c81ca9211bd34f45f14dd4dbf268177764929171
Agustin Pecci <agustinpecci@gmail.com>
2024-04-12 12:05:44 -0300

    fix(release.config.js): fix @semantic-release/git

--- release.config.js
+++ release.config.js
@@ -11,7 +11,14 @@ const config = {
         pkgRoot: "dist",
       },
     ],
-    "@semantic-release/git",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ],
 };
 
