ab48a8c294af29395144a8fcae6e6132504419e8
Agustin Pecci <agustinpecci@gmail.com>
2024-04-12 15:15:42 -0300

    fix(release.config.js): test another config

--- release.config.js
+++ release.config.js
@@ -19,14 +19,7 @@ const config = {
         prepareCmd: "npm run postversion",
       },
     ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    "@semantic-release/git",
   ],
 };
 
