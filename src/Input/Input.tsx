0469d00e5594d6559ae9b590cf94831cec7c8753
Agustin Pecci <agustinpecci@gmail.com>
2024-06-10 18:46:32 -0300

    fix(input): remove useEffect

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Input as NextUiInput } from "@nextui-org/react";
 import { InputProps } from "./types";
 
