7eba36e3ee07bad1a6fab1265c52933a3e5f91a3
danielavazquez95 <danielavazquez95@gmail.com>
2024-07-04 13:44:10 -0300

    feat(component): added Switch component
    BREAKING CHANGE: added Switch component

---
+++ src/stories/Switch.stories.ts
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import Switch from "../Switch/Switch";
+
+const meta = {
+    title: "Components/Switch",
+    component: Switch,
+    parameters: {
+        layout: "centered",
+    },
+    args: {
+        onValueChange: fn(),
+    },
+    argTypes: {
+      isLoading: { control: "boolean" },
+      isDisabled: { control: "boolean" },
+      defaultSelected: { control: "boolean" },
+    },
+} satisfies Meta<typeof Switch>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        isLoading: false,
+    },
+  };
+
+export const DefaultSelected: Story = {
+    args: {
+        defaultSelected: true,
+    },
+};
+  
+export const IsDisabled: Story = {
+    args: {
+        isDisabled: true,
+    },
+  };
+
+export const IsLoading: Story = {
+    args: {
+      isLoading: true,
+    },
+};
