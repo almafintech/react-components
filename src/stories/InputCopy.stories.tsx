b353dc55c5f425af5f79d1ddc2a373d5f04bc171
Abi Rivas <lourdes.2502@gmail.com>
2024-08-20 15:31:29 -0300

    feat(inputcopy): new component inputCopy

---
+++ src/stories/InputCopy.stories.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import InputCopy from "../InputCopy/InputCopy";
+import { fn } from "@storybook/test";
+
+const meta = {
+  title: "Components/InputCopy",
+  component: InputCopy,
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    onClick: fn(),
+  },
+  decorators: [
+    (Story) => (
+      <div className="w-96">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof InputCopy>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    value: "https://www.google.com",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    value: "https://www.google.com",
+    disabled: true,
+  },
+};
