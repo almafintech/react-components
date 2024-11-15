9a1e6a4b515fb0ef77bce647219848d1776501ce
Agustin Pecci <agustinpecci@gmail.com>
2024-11-15 14:45:21 -0300

    feat: update commit analyzer to release on style and refactor too

--- release.config.js
+++ release.config.js
@@ -3,7 +3,16 @@ const path = require("path");
 const config = {
   branches: ["main", "next", "beta"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { type: "style", release: "patch" },
+          { type: "refactor", release: "patch" },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/github",
