7215c91673bcdb5fe05b15c5573be03e1d6863f1
Ramiro Basile <ramirobasile1@gmail.com>
2024-11-25 11:57:18 -0300

    refactor(inputphone): have InputPhone label passed as prop

--- src/InputPhone/InputPhone.tsx
+++ src/InputPhone/InputPhone.tsx
@@ -14,6 +14,7 @@ import { isByma } from "../utils";
 
 const InputPhone = (props: InputPhoneProps) => {
   const {
+    label = "Teléfono",
     description,
     infoMessage,
     errorMessage,
@@ -34,7 +35,7 @@ const InputPhone = (props: InputPhoneProps) => {
   const isBymaTheme = isByma(theme);
 
   const {
-    label,
+    label: labelStyle,
     helperWrapper,
     description: descriptionStyle,
     infoMessage: infoMessageStyle,
@@ -56,8 +57,8 @@ const InputPhone = (props: InputPhoneProps) => {
 
   return (
     <div className={isBymaTheme ? "byma" : ""}>
-      <label className={`${label} ${isFormField ? formField : ""}`}>
-        Teléfono
+      <label className={`${labelStyle} ${isFormField ? formField : ""}`}>
+        {label}
       </label>
       <PhoneInput
         {...rest}
